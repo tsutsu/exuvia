4b9993f62574441aa83d83e573db5f084591671d
Levi Aul <levi@leviaul.com>
2017-01-05 16:22:32 -0800

    Add Hex package administrivia

--- mix.exs
+++ mix.exs
@@ -6,10 +6,26 @@ defmodule Exuvia.Mixfile do
   def project do [
     app: :exuvia,
     version: @version,
+    description: description(),
+    package: package(),
     elixir: "~> 1.4",
     build_embedded: Mix.env == :prod,
     start_permanent: Mix.env == :prod,
     deps: deps()
+  ] end
+
+  defp description do
+    """
+    Exuvia abstracts away everything needed to connect to your Elixir node, via both SSH and the distribution protocol.
+    """
+  end
+
+  defp package do [
+    name: :exuvia,
+    files: ["lib", "config", "mix.exs", "VERSION", "README.md", "LICENSE"],
+    maintainers: ["Levi Aul"],
+    licenses: ["BSD"],
+    links: %{"GitHub" => "https://github.com/meetwalter/exuvia"}
   ] end
 
   def application do [
@@ -19,6 +35,7 @@ defmodule Exuvia.Mixfile do
 
   defp deps do [
     {:temp, "~> 0.4.1"},
-    {:tentacat, "~> 0.5.3"}
+    {:tentacat, "~> 0.5.3"},
+    {:ex_doc, ">= 0.0.0", only: :dev}
   ] end
 end
